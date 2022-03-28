aabd23101bff259f3b2291e3d7ba2043c8b1298c
yejin-won <100192779+yejin-won@users.noreply.github.com>
2022-03-28 23:03:35 +0900

    fisrt commit

--- ToDo/src/main/java/com/javaproject/todo/command/TListCommand.java
+++ ToDo/src/main/java/com/javaproject/todo/command/TListCommand.java
@@ -7,8 +7,10 @@ public class TListCommand implements TCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stubs
+		BDao dao = new BDao();
+		ArrayList<BDto> dtos =dao.list();
+		request.setAttribute("list", dtos);
 	}
 
 }
